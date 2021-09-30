9404b0601bed2e76eb058e77b6c102dbe2c97745
Hanif Watson <jeffrey.h.watson@protonmail.com>
2021-09-29 21:15:31 -0700

    cleanup

--- src/functions.py
+++ src/functions.py
@@ -1,23 +1,4 @@
-# global variables
-
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-from statsmodels.formula.api import ols
-from statsmodels.stats.diagnostic import linear_rainbow, het_breuschpagan
-from statsmodels.stats.outliers_influence import variance_inflation_factor
-
-def create_formula(target_name, df):
-    """Returns a formula string.
-    
-    Args:
-        target_name: A string containing the target feature.
-        df: A data frame.
-    Returns:
-        A string containing an ols model formula.
-        """
-    features = df.drop(target_name, axis=1).columns
-    features = "+".join(features)
-    return target_name + "~" + features
+from statsmodels.stats.diagnostic import linear_rainbow
 
 def rainbow(model_results):
     """Returns the statistic and p-value of a rainbow test.
